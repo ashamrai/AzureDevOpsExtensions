3b6ebc3bb01bb4bf7d865d40ffd08e14333f4e98
Aleksandr Shamrai <oshamrai@KV56>
2020-01-31 11:17:51 +0200

    powershell build tasks to copy last changed files

---
+++ CustomPSTasks/CopyChangesFromLastCommit.ps1
@@ -0,0 +1,24 @@
+﻿$targetfolder = "$(Build.StagingDirectory)" + "/"
+
+function CopyFiles{
+    param( [string]$source )
+
+    $target = $targetfolder + $source
+
+    New-Item -Force $target
+    copy-item $source $target -Force
+}
+
+$changes = git diff --name-only --relative --diff-filter AMR HEAD^ HEAD .
+
+if ($changes -is [string]){ CopyFiles $changes }
+else
+{
+    if ($changes -is [array])
+    {       
+        foreach ($change in $changes){ CopyFiles $change }
+    }
+}
+
+
+ 
---
+++ CustomPSTasks/CopyChangesFromLastTag.ps1
@@ -0,0 +1,22 @@
+﻿$targetfolder = "$(Build.StagingDirectory)" + "/"
+
+function CopyFiles{
+    param( [string]$source )
+
+    $target = $targetfolder + $source
+
+    New-Item -Force $target
+    copy-item $source $target -Force
+}
+
+$last_tag = git describe --tags --abbrev=0 --match "[0-9]*"
+$changes = git diff --name-only --relative --diff-filter AMR $last_tag HEAD .
+
+if ($changes -is [string]){ CopyFiles $changes }
+else
+{
+    if ($changes -is [array])
+    {       
+        foreach ($change in $changes){ CopyFiles $change }
+    }
+}
