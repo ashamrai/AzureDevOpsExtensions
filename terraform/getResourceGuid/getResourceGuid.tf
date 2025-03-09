4641cef56872c387ff27125ebc5ccbf39b3fd590
Oleksandr Shamrai <ashamrai@outlook.com>
2025-03-09 17:35:30 +0200

    Update getResourceGuid.tf

--- terraform/getResourceGuid/getResourceGuid.tf
+++ terraform/getResourceGuid/getResourceGuid.tf
@@ -1,6 +1,14 @@
+#define the following var:
+# sbID - subscrption Id
+# rgName - resource group name
+# resName - resource name
+# appId - MS Entra ID application
+# appToken - application secret
+# appTenantId - MS Entra ID
+
 resource "null_resource" "get_resource_giud" {
   provisioner "local-exec" {
-  command = "pwsh -File ./modules/module-azure-resource-guid/getResourceGuid.ps1 -sbID ${var.sbID} -rgName ${var.rgName} -resName ${var.resName} -appId ${var.appId} -appToken ${var.appToken} -appTenantId ${app.TenantId}"
+  command = "pwsh -File ./modules/module-azure-resource-guid/getResourceGuid.ps1 -sbID ${var.sbID} -rgName ${var.rgName} -resName ${var.resName} -appId ${var.appId} -appToken ${var.appToken} -appTenantId ${var.appTenantId}"
 }
 
 data "local_file" "resource_guid_file" {
