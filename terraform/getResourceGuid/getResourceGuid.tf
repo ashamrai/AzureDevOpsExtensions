45055b761786be21e3aff47f1d176215d0c0c49c
Oleksandr Shamrai <ashamrai@outlook.com>
2025-03-09 17:31:15 +0200

    Update getResourceGuid.tf

--- terraform/getResourceGuid/getResourceGuid.tf
+++ terraform/getResourceGuid/getResourceGuid.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "get_resource_giud" {
   provisioner "local-exec" {
-  command = "pwsh -File ./modules/module-azure-resource-guid/getResourceGuid.ps1 -sbID ${var.sbID} -rgName ${var.rgName} -resName ${var.resName} -appId ${var.appId} -appToken {var.appToken} -appTenantId ${app.TenantId}"
+  command = "pwsh -File ./modules/module-azure-resource-guid/getResourceGuid.ps1 -sbID ${var.sbID} -rgName ${var.rgName} -resName ${var.resName} -appId ${var.appId} -appToken ${var.appToken} -appTenantId ${app.TenantId}"
 }
 
 data "local_file" "resource_guid_file" {
