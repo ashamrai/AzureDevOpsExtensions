66f502cd97144846abaf0c3f6840bc948673eca4
Oleksandr Shamrai <ashamrai@outlook.com>
2025-03-09 17:30:32 +0200

    Create getResourceGuid.tf

---
+++ terraform/getResourceGuid/getResourceGuid.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "get_resource_giud" {
+  provisioner "local-exec" {
+  command = "pwsh -File ./modules/module-azure-resource-guid/getResourceGuid.ps1 -sbID ${var.sbID} -rgName ${var.rgName} -resName ${var.resName} -appId ${var.appId} -appToken {var.appToken} -appTenantId ${app.TenantId}"
+}
+
+data "local_file" "resource_guid_file" {
+  filename   = "./modules/module-azure-resource-guid/resourceGuid.txt"
+  depends_on = [null_resource.get_resource_giud]
+}
+
+# read the guid through: data.local_file.resource_guid_file.content
